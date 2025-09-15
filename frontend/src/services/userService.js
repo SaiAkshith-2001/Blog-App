7f584e5fc44960d07e898d048dafc5c306bb8f96
Sai\Akshith-2001 <saiakshithanumalla@gmail.com>
2025-09-15 21:52:41 +0530

    [FE]: added logout route

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,6 +2,7 @@ import { axiosInstance } from "./serviceConfig";
 
 const userService = {
   login: (payload) => axiosInstance.post(`/api/user/login`, payload),
+  logout: () => axiosInstance.post(`/api/user/logout`),
   register: (payload) => axiosInstance.post(`/api/user/register`, payload),
   getUserProfileById: (id) => axiosInstance.get(`/api/user/profile/${id}`),
 };
