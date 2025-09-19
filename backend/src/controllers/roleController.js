5c0034892b56b3125018f7dbf96d96d034cb4852
Sai\Akshith-2001 <saiakshithanumalla@gmail.com>
2025-09-19 16:25:23 +0530

    [BE]: updated role controller to manage roleCodes using createRole

--- backend/src/controllers/roleController.js
+++ backend/src/controllers/roleController.js
@@ -1,4 +1,18 @@
-import { Role } from "../models/role.Schema.js";
+import { Role, ROLES } from "../models/role.Schema.js";
+
+export const createRole = async (userId, role = ROLES.user, status = true) => {
+  try {
+    const roleCode = await Role.create({
+      userId,
+      role,
+      status,
+    });
+    return roleCode;
+  } catch (error) {
+    console.error("something went wrong", error);
+  }
+};
+
 export const getRole = async (role) => {
   try {
     const roleDoc = await Role.findOne({ role: role, status: true });
